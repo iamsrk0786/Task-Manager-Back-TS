b507e55ff64b4b559f5bf5df2b68d1bc4ccdbf05
ShahRukh Khan <164313837+iamsrk0786@users.noreply.github.com>
2024-11-16 18:17:20 +0530

    Update task.ts

--- src/services/task.ts
+++ src/services/task.ts
@@ -8,6 +8,15 @@ export const createTask = async (taskData: Partial<ITask>): Promise<ITask> => {
   const task = new Task(taskData);
   return task.save();
 };
+export const updateTask = async (
+  id: string,
+  taskData: Partial<ITask>
+): Promise<ITask | null> => {
+  return Task.findByIdAndUpdate(id, taskData, {
+    new: true,
+    runValidators: true,
+  });
+};
 
 export const deleteTask = async (taskId: string): Promise<ITask | null> => {
   return Task.findByIdAndDelete(taskId);
