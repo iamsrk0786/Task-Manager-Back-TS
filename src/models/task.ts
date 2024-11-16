71f51e7d414a2583737bc28152fb63c6d34bd71c
ShahRukh Khan <164313837+iamsrk0786@users.noreply.github.com>
2024-11-16 18:14:33 +0530

    Update task.ts

--- src/models/task.ts
+++ src/models/task.ts
@@ -3,13 +3,18 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface ITask extends Document {
   title: string;
   description: string;
+  priority: 'High' | 'Medium' | 'Low'; 
   completed: boolean;
+  dueDate?: Date;
+
 }
 
 const taskSchema: Schema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Medium' }, // Default priority
   completed: { type: Boolean, default: false },
+  dueDate: { type: Date },
 });
 
 const Task = mongoose.model<ITask>('Task', taskSchema);
